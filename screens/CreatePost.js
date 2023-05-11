f5fea2c0a2af7158d75d454793c954cdbf133f9d
Rafael-uro <94479855+Rafael-uro@users.noreply.github.com>
2023-05-11 13:57:19 -0300

    Add files via upload

---
+++ screens/CreatePost.js
@@ -0,0 +1,175 @@
+import * as React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+  ScrollView,
+  TextInput,
+  Dimensions,
+  Button,
+  Alert,
+} from "react-native";
+import DropDownPicker from "react-native-dropdown-picker";
+import { RFValue } from "react-native-responsive-fontsize";
+
+export default class CreatePost extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false,
+      previewImage: "image_1",
+      dropdownHeight: 40,
+    };
+  }
+  render() {
+    if (this.state.previewImage) {
+      let preview_images = {
+        image_1: require("../assets/image_1.jpg"),
+        image_2: require("../assets/image_2.jpg"),
+        image_3: require("../assets/image_3.jpg"),
+        image_4: require("../assets/image_4.jpg"),
+        image_5: require("../assets/image_5.jpg"),
+      };
+      return (
+        <View style={styles.container}>
+          <SafeAreaView style={styles.droidSafeArea} />
+          <View style={styles.appTitle}>
+            <View style={styles.appIcon}>
+              <Image
+                source={require("../assets/logo.png")}
+                style={styles.iconImage}
+              ></Image>
+            </View>
+            <View style={styles.appTitleTextContainer}>
+              <Text style={styles.appTitleText}>New Story</Text>
+            </View>
+          </View>
+          <View style={styles.fieldsContainer}>
+            <Image
+              source={preview_images[this.state.previewImage]}
+              style={styles.previewImage}
+            ></Image>
+            <View style={{ height: RFValue(this.state.dropdownHeight) }}>
+              <DropDownPicker
+                items={[
+                  { label: "Image 1", value: "image_1" },
+                  { label: "Image 2", value: "image_2" },
+                  { label: "Image 3", value: "image_3" },
+                  { label: "Image 4", value: "image_4" },
+                  { label: "Image 5", value: "image_5" },
+                ]}
+                defaultValue={this.state.previewImage}
+                open={this.state.dropdownHeight == 170 ? true : false}
+                onOpen={() => {
+                  this.setState({ dropdownHeight: 170 });
+                }}
+                onClose={() => {
+                  this.setState({ dropdownHeight: 40 });
+                }}
+                style={{
+                  backgroundColor: "transparent",
+                  borderWidth: 1,
+                  borderColor: "white",
+                }}
+                textStyle={{
+                  color: this.state.dropdownHeight == 170 ? "black" : "white",
+                }}
+                onSelectItem={(item) =>
+                  this.setState({
+                    previewImage: item.value,
+                  })
+                }
+              />
+            </View>
+            <ScrollView>
+              <TextInput
+                style={styles.inputFont}
+                onChangeText={(title) => this.setState({ title })}
+                placeholder={"Title"}
+                placeholderTextColor="white"
+              />
+
+              <View style={styles.submitButton}>
+                <Button
+                  onPress={() => this.addStory()}
+                  title="Submit"
+                  color="#841584"
+                />
+              </View>
+            </ScrollView>
+          </View>
+          <View style={{ flex: 0.08 }} />
+        </View>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#190909",
+  },
+  droidSafeArea: {
+    marginTop:
+      Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35),
+  },
+  appTitle: {
+    flex: 0.07,
+    flexDirection: "row",
+  },
+  appIcon: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  iconImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  appTitleTextContainer: {
+    flex: 0.7,
+    justifyContent: "center",
+  },
+  appTitleText: {
+    color: "white",
+    fontSize: RFValue(28),
+  },
+  fieldsContainer: {
+    flex: 0.85,
+  },
+  previewImage: {
+    width: "93%",
+    height: RFValue(250),
+    alignSelf: "center",
+    borderRadius: RFValue(10),
+    marginVertical: RFValue(10),
+    resizeMode: "contain",
+  },
+  inputFont: {
+    height: RFValue(40),
+    marginTop: RFValue(40),
+    borderColor: "white",
+    borderWidth: RFValue(1),
+    borderRadius: RFValue(10),
+    paddingLeft: RFValue(10),
+    color: "white",
+  },
+  inputFontExtra: {
+    marginTop: RFValue(15),
+  },
+  inputTextBig: {
+    textAlignVertical: "top",
+    padding: RFValue(5),
+  },
+  submitButton: {
+    marginTop: RFValue(20),
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
